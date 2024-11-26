6a5568415d85783155fe617f5cb60df94729057f
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-11-26 13:28:39 +0530

    ..

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+import numpy as np
+from joblib import load
+
+load_model = load(r'C:\Users\LENOVO\Desktop\p\static\model.pk')
 
 app = Flask(__name__)
 app.secret_key = 'supersecretmre'
@@ -16,6 +20,25 @@ def about():
 
 @app.route('/analysis', methods=['GET','POST'])
 def analysis():
+    if request.method=='POST':
+        age = request.form.get('age')
+        inc = request.form.get('inc')
+        score = request.form.get('score')
+        #print(age,inc,score)
+        inp=np.array([age,inc,score]).reshape(1,-1)
+        pred=load_model.predict(inp)[0]
+        #print(pred)
+        if pred==0:
+            output='Customers with low income & low expenditure'
+        elif pred==1:
+            output='Customers with high income & high expenditure'
+        elif pred==2:
+            output='Customers with medium income & medium expenditure'
+        elif pred==3:
+            output='Customers with high income & low expenditure'
+        elif pred==4:
+            output='Customers with low income & high expenditure'
+        return render_template('analysis.html',output=output)
     return render_template('analysis.html')
 
 @app.route('/results', methods=['GET','POST'])
